f5ed6f94c86366c9b15a98e7646ac38a8b3dc38a
ferutizi <fer.utizi@gmail.com>
2022-03-13 21:47:20 -0300

    estilo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
-function App() {
+const App = () => {
+
+  const [contador, setContador] = useState(0);
+
+  const aumentar = () => {
+      setContador(contador + 1);
+  }
+
+    const disminuir = () => {
+      setContador(contador - 1);
+  }
+
+    const sumar10 = () => {
+      setContador(contador + 10);
+  }
+
+    const porDos = () => {
+      setContador(contador * 2);
+  }
+
+    const reset = () => {
+      setContador(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container py-5 bg-light text-center">
+      
+        <h1>CONTADOR</h1>
+
+        <button onClick={aumentar} className="btn btn-primary mx-4">
+          Aumentar
+        </button>
+
+        <button onClick={disminuir} className="btn btn-danger">
+          Disminuir
+        </button>
+
+        <button onClick={sumar10} className="btn10 btn btn-warning ms-5 me-3">
+          +10
+        </button>
+
+        <button onClick={porDos} className="btn2 btn btn-warning">
+          X2
+        </button>
+
+        <h3 className="p-4">{contador}</h3>
+
+        <button onClick={reset} className="btnReset btn btn-secondary">
+          Reset
+        </button>
+      </div>
+    </>
   );
 }
 
