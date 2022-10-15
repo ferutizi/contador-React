d9be6b7b23505d75939244f5f1813cda8c682d2c
ferutizi <fer.utizi@gmail.com>
2022-10-14 21:00:49 -0300

    implementando unit test con testing library

--- src/App.js
+++ src/App.js
@@ -5,50 +5,55 @@ const App = () => {
   const [contador, setContador] = useState(0);
 
   const aumentar = () => {
-      setContador(contador + 1);
+    setContador(contador + 1);
   }
-
-    const disminuir = () => {
-      setContador(contador - 1);
+  const disminuir = () => {
+    setContador(contador - 1);
   }
-
-    const sumar10 = () => {
-      setContador(contador + 10);
+  const sumar10 = () => {
+    setContador(contador + 10);
   }
-
-    const porDos = () => {
-      setContador(contador * 2);
+  const porDos = () => {
+    setContador(contador * 2);
   }
-
-    const reset = () => {
-      setContador(0);
+  const reset = () => {
+    setContador(0);
   }
 
   return (
     <>
-      <div className="container py-5 bg-light text-center">
-      
+      <div className="container py-5 bg-light text-center">      
+        <p>hola</p>
         <h1>CONTADOR</h1>
-
-        <button onClick={aumentar} className="btn btn-primary mx-4">
+        <button
+          data-testid="aumentar"
+          onClick={aumentar}
+          className="btn btn-primary mx-4">
           Aumentar
         </button>
-
-        <button onClick={disminuir} className="btn btn-danger">
+        <button
+          data-testid="disminuir"
+          onClick={disminuir}
+          className="btn btn-danger">
           Disminuir
         </button>
-
-        <button onClick={sumar10} className="btn10 btn btn-warning ms-5 me-3">
+        <button
+          data-testid="sumar10"
+          onClick={sumar10}
+          className="btn10 btn btn-warning ms-5 me-3">
           +10
         </button>
-
-        <button onClick={porDos} className="btn2 btn btn-warning">
+        <button
+          data-testid="porDos"
+          onClick={porDos}
+          className="btn2 btn btn-warning">
           X2
         </button>
-
         <h3 className="p-4">{contador}</h3>
-
-        <button onClick={reset} className="btnReset btn btn-secondary">
+        <button
+          data-testid="reset"
+          onClick={reset}
+          className="btnReset btn btn-secondary">
           Reset
         </button>
       </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,57 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+/* test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-});
+}); */
+beforeEach(() => render(<App />));
+
+describe('initial elements render', () => {
+  test('there must be a header',() => {
+    expect(screen.getByRole('heading', {level: 1} )).toBeInTheDocument();
+    expect(screen.getByText(/contador/i)).toBeInTheDocument();
+  });
+  
+  test('the counter must start at zero', () => {
+    expect(screen.getByText(0)).toBeInTheDocument();
+  });
+  
+  test('there should be five buttons', () => {
+    expect(screen.getAllByRole('button'));
+  });
+})
+
+describe('buttons function', () => {
+  //Falta fireEvents
+  test('when press aumentar, the counter must add 1', () => {
+    const btnAumentar = screen.getByTestId('aumentar');
+  
+    expect(btnAumentar).toBeInTheDocument();
+  }); 
+  
+  test('when press disminuir, the counter must substract 1', () => {
+    const btnDisminuir = screen.getByTestId('disminuir');
+  
+    expect(btnDisminuir).toBeInTheDocument();
+  }); 
+  
+  test('when press sumar10, the counter must add 10', () => {
+    const btnSumar10 = screen.getByTestId('sumar10');
+  
+    expect(btnSumar10).toBeInTheDocument();
+  }); 
+  
+  test('when press porDos, the counter must multiplicate for 2', () => {
+    const btnPorDos = screen.getByTestId('porDos');
+  
+    expect(btnPorDos).toBeInTheDocument();
+  }); 
+  
+  test('when press reset, the counter must be reset', () => {
+    const btnReset = screen.getByTestId('reset');
+  
+    expect(btnReset).toBeInTheDocument();
+  }); 
+})
